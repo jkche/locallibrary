78ad395d8f33df9c7d99e483b22979ac61076c32
jkcheung <justinkcheung@gmail.com>
2019-01-10 11:40:08 -0500

    updated through part 9 (added forms, generic views)

--- catalog/urls.py
+++ catalog/urls.py
@@ -13,3 +13,22 @@ urlpatterns = [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
 ]
+
+# URL config for renew-books page
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+# URL config for Author GenericView pages
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete')
+]
+
+# URL config for Book GenericView pages
+urlpatterns += [
+    path('book/create/', views.BookCreate.as_view(), name='book_create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete')
+]
